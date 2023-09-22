3cac6db8079909d32d02ad0726e9059454ab4403
JanJezewski01 <jezewski533@gmail.com>
2023-09-22 02:45:02 +0200

    add new element

--- app.module.ts
+++ app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
