0042514fd44cf07ba1aa5e0370728fa792760fdf
JanJezewski01 <jezewski533@gmail.com>
2023-11-02 05:04:56 +0100

    add new function

--- app.module.ts
+++ app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { DroptownComponent } from './droptown/droptown.component';
-import { GraficComponent } from './grafic/grafic.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    NavbarComponent,
-    DroptownComponent,
-    GraficComponent
+    
+   
   ],
   imports: [
     BrowserModule
--- calendar/calendar.component.ts
+++ calendar/calendar.component.ts
@@ -7,14 +7,14 @@ import { Component, Type } from '@angular/core';
 })
 
 export class CalendarComponent {
-  config: { date: string; Day: string[]; 
+  config: { date: string; Day: string[];  WeekDays: number[];
     Monday: number[]; Tuesday: number[]; Wednesday: number[]; Thursday: number[]; Friday: number[]; Saturday: number[]; SunDay: number[];};
   
   constructor() {
     this.config = {
       date: new Date().toDateString(),
       Day: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-     // WeekDays: [1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+      WeekDays: [1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
       
       Monday: [2,9,16,23,30],
       Tuesday: [3,10,17,24,31],
