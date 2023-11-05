a1012b018684b9190f455daddfcda6d3cf4e0e5c
JanJezewski01 <jezewski533@gmail.com>
2023-11-05 23:40:58 +0100

    new function

--- app.module.ts
+++ app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { WeatherComponent } from './weather/weather.component';
 
 
 
@@ -9,6 +10,7 @@ import { CalendarComponent } from './calendar/calendar.component';
   declarations: [
     AppComponent,
     CalendarComponent,
+    WeatherComponent,
     
    
   ],
--- calendar/calendar.component.ts
+++ calendar/calendar.component.ts
@@ -16,13 +16,13 @@ export class CalendarComponent {
       Day: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
       WeekDays: [1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
       
-      Monday: [2,9,16,23,30],
-      Tuesday: [3,10,17,24,31],
-      Wednesday: [4,11,18,25],
-      Thursday: [5,12,19,26],
-      Friday: [6,13,20,27],
-      Saturday: [7,14,21,28],
-      SunDay: [1,8,15,22,29]
+      Monday: [6,13,20,27],
+      Tuesday: [7,14,21,28],
+      Wednesday: [1,8,15,22,29],
+      Thursday: [2,9,16,23,30],
+      Friday: [3,10,17,24],
+      Saturday: [4,11,18,25],
+      SunDay: [5,12,19,26]
     };
     
   }
